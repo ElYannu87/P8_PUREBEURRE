47c5ceebc8584eeeeb481b4d7b2b213c67fa5204
ElYannu87 <y.driever@gmail.com>
2021-02-03 12:03:11 +0100

    added URLS and config for views in pages

--- purebeurre/settings.py
+++ purebeurre/settings.py
@@ -126,6 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = (os.path(BASE_DIR, "static"),)
+# STATICFILES_DIRS = (os.path(BASE_DIR, "static"),)
