5b3fa56f8e27f84e0a0d7c8e002a602698961729
ElYannu87 <y.driever@gmail.com>
2021-04-06 10:54:07 +0200

    travis changes

---
+++ travis.py
@@ -0,0 +1,11 @@
+from . import *
+ 
+
+ DATABASE = {
+     'default':'django.db.backends.postgresql',
+     'NAME': '',
+     'USER': 'postgres',
+     'PASSWORD': '',
+     'HOST': '',
+     'PORT': '',
+ }
